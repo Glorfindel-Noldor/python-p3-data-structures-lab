d816677f1dc01d12dbeadd398ea83475a5df9f82
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-08 18:54:14 -0500

    data structures

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,35 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food['name'] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy_foods = [food for food in spicy_foods if food['heat_level'] > 5]
+    return spicy_foods;
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+        heat_number = item["heat_level"] * "🌶"  
+        print( f"{item['name']} ({item['cuisine']}) | Heat Level: {heat_number}" )
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for item in spicy_foods:
+        if item['cuisine'] == cuisine:
+            return item
+    return None 
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            heat_level = '🌶' * food['heat_level']
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food['heat_level'] for food in spicy_foods)
+    average_heat = total_heat / len(spicy_foods) if spicy_foods else 0
+    return round(average_heat)
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def create_spicy_food(spicy_foods, new_spicy_food):
+    spicy_foods.append(new_spicy_food)
+    return spicy_foods
